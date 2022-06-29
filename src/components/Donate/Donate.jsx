d1150fb272248edf5252c353dfec260b2ed1f62e
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-06-28 22:45:55 -0500

    updated the donation page

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -14,6 +14,14 @@ const Donate = () => {
     window.open('https://futurefornature.org/donate', '_blank');
   }
 
+  const saveTheBoundaryWaters = () => {
+    window.open('https://www.savetheboundarywaters.org/donate', '_blank');
+  }
+
+  const treesWaterPeople = () => {
+    window.open('https://treeswaterpeople.org/donate', '_blank');
+  }
+
   const worldLandTrust = () => {
     window.open('https://www.worldlandtrust.org/product/action-fund/', '_blank');
   }
@@ -35,8 +43,17 @@ const Donate = () => {
               <Link className='donateLink' to='/donate' onClick={coolEarth}>
                   Cool Earth
               </Link>
-              <p className='donateDescription'>From their website: "We back people 
-              to protect rainforest and fight the climate crisis."</p>
+              <p className='donateDescription'>From their website: "We champion 
+              the relationship between people, rainforest and climate.
+              <br></br>
+              <br></br>
+              Cool Earth exists to give cash direct to rainforest communities, 
+              to fund projects that create choice, tackle the root causes of 
+              deforestation and protect vital carbon sinks.
+              <br></br>
+              <br></br>
+              This is our mission. To back people, to protect the rainforest 
+              and fight the climate crisis."</p>
           </div>
           <div className='donateOrg'>
               <Link className='donateLink' to='/donate' onClick={futureForNature}>
@@ -48,6 +65,34 @@ const Donate = () => {
               individuals is what will make the difference for the future of nature. 
               Through their leadership they inspire and mobilize communities, 
               organizations, governments, investors and the public at large."</p>
+          </div>
+          <div className='donateOrg'>
+              <Link className='donateLink' to='/donate' onClick={saveTheBoundaryWaters}>
+                  Save the Boundary Waters
+              </Link>
+              <p className='donateDescription'>From their website: "Northeastern 
+              Minnesotans for Wilderness (NMW), which leads the Campaign to Save 
+              the Boundary Waters, was formed in 1996 to continue working to protect 
+              the Boundary Waters and other wild places against ever-increasing public 
+              and commercial pressures so that the area’s wild character remains intact 
+              for future generations. In September 2013, the national coalition, called 
+              the Campaign to Save the Boundary Waters, was formed to create a national 
+              movement to protect the Boundary Waters from sulfide-ore copper mining. 
+              It is led and funded by NMW. 
+              <br></br>
+              <br></br>
+              Today, NMW is the largest conservation organization in Minnesota that 
+              focuses on protecting and preserving the Boundary Waters and greater 
+              Quetico-Superior region."</p>
+          </div>
+          <div className='donateOrg'>
+              <Link className='donateLink' to='/donate' onClick={treesWaterPeople}>
+                  Trees, Water, People
+              </Link>
+              <p className='donateDescription'>From their website: "Our mission is 
+              to improve people's lives by helping communities protect, conserve, 
+              and manage the natural resources upon which their long-term well-being 
+              depends."</p>
           </div>
           <div className='donateOrg'>
               <Link className='donateLink' to='/donate' onClick={worldLandTrust}>
