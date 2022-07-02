3d9f63f7d5b547103f5ca0e479150e9ab1bed318
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-07-02 11:26:54 -0500

    removed the link functions

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -15,30 +15,6 @@ const Donate = () => {
       dispatch({ type: 'GET_ORGANIZATIONS' });
   }, [dispatch]);
 
-  const coolEarth = () => {
-    window.open('https://www.coolearth.org/donate/', '_blank');
-  }
-
-  const futureForNature = () => {
-    window.open('https://futurefornature.org/donate', '_blank');
-  }
-
-  const saveTheBoundaryWaters = () => {
-    window.open('https://www.savetheboundarywaters.org/donate', '_blank');
-  }
-
-  const treesWaterPeople = () => {
-    window.open('https://treeswaterpeople.org/donate', '_blank');
-  }
-
-  const worldLandTrust = () => {
-    window.open('https://www.worldlandtrust.org/product/action-fund/', '_blank');
-  }
-
-  const worldWildlifeFund = () => {
-    window.open('https://support.worldwildlife.org/site/SPageServer?pagename=main_monthly&s_src=AWE2209OQ18299A01179RX&s_subsrc=topnav', '_blank');
-  }
-  
   return (
     <div className='donateItem'>
         <div className='donateHeader'>
--- src/components/Donate/Organization.jsx
+++ src/components/Donate/Organization.jsx
@@ -7,7 +7,6 @@ import './Donate.css';
 const Organization = (props) => {
 
     const goToOrg = (link) => {
-        console.log(link)
         window.open(encodeURI(link), '_blank');
       }
 
