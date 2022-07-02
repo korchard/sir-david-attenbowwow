f69c827455a3685dacfe9951b1367dd326501df5
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-07-02 09:50:21 -0500

    updated the server.js to include the org router and route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const contactRouter = require('./routes/contact.router');
 const galleryRouter = require('./routes/gallery.router');
+const organizationsRouter = require('./routes/organizations.router');
 
 // MIDDLEWARE
 app.use(bodyParser.json());
@@ -14,6 +15,7 @@ app.use(express.static('build'));
 // ROUTES
 app.use('/api/contact', contactRouter);
 app.use('/api/gallery', galleryRouter);
+app.use('/api/organizations', organizationsRouter);
 
 // LISTEN
 app.listen(PORT, () => {
