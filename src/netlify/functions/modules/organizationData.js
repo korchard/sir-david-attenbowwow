b0e8787fe336ce009e0102602307a99a2da4de71
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-07-22 15:34:07 -0500

    created backend for netlify

---
+++ src/netlify/functions/modules/organizationData.js
@@ -0,0 +1,68 @@
+const organizationData = [
+    {   
+        id: 1,
+        title: 'Cool Earth', 
+        description: `We champion the relationship between people, rainforest 
+        and climate. Cool Earth exists to give cash direct to rainforest communities, 
+        to fund projects that create choice, tackle the root causes of deforestation 
+        and protect vital carbon sinks. This is our mission. To back people, to 
+        protect the rainforest and fight the climate crisis. `, 
+        link: 'https://www.coolearth.org/donate/'
+    }, 
+    {
+        id: 2,
+        title: `Future For Nature`, 
+        description: `Future For Nature supports young, talented and ambitious 
+        conservationists committed to protecting species of wild animals and plants. 
+        The commitment of these individuals is what will make the difference for the 
+        future of nature. Through their leadership they inspire and mobilize communities, 
+        organizations, governments, investors and the public at large.`, 
+        link: 'https://futurefornature.org/donate'
+    }, 
+    {
+        id: 3,
+        title: 'Save the Boundary Waters', 
+        description: `Northeastern Minnesotans for Wilderness (NMW), which leads the 
+        Campaign to Save the Boundary Waters, was formed in 1996 to continue working 
+        to protect the Boundary Waters and other wild places against ever-increasing 
+        public and commercial pressures so that the area’s wild character remains intact 
+        for future generations. In September 2013, the national coalition, called 
+        the Campaign to Save the Boundary Waters, was formed to create a national 
+        movement to protect the Boundary Waters from sulfide-ore copper mining. 
+        It is led and funded by NMW. Today, NMW is the largest conservation organization 
+        in Minnesota that focuses on protecting and preserving the Boundary Waters and 
+        greater Quetico-Superior region.`, 
+        link: 'https://www.savetheboundarywaters.org/donate'
+    }, 
+    {
+        id: 4,
+        title: 'Trees, Water, People', 
+        description: `Our mission is to improve people's lives by helping communities 
+        protect, conserve, and manage the natural resources upon which their long-term 
+        well-being depends.`, 
+        link: 'https://treeswaterpeople.org/donate'
+    }, 
+    {
+        id: 5,
+        title: `World Land Trust`, 
+        description: `World Land Trust (WLT) is an international conservation charity 
+        that protects the world’s most biologically significant and threatened habitats. 
+        Working through a network of partner organisations around the world, WLT funds 
+        the creation of reserves and provides permanent protection for habitats and wildlife. 
+        Partnerships are developed with established and highly respected local 
+        organisations who engage support and commitment among the local community.`, 
+        link: 'https://www.worldlandtrust.org/product/action-fund/'
+    }, 
+    {
+        id: 6,
+        title: `World Wildlife Fund`, 
+        description: `Our vision is to build a future in which people live in harmony with 
+        nature. To deliver this mission, we work to conserve and restore biodiversity, the 
+        web that supports all life on Earth; to reduce humanity’s environmental footprint; 
+        and to ensure the sustainable use of natural resources to support current 
+        and future generations.`, 
+        link: 'https://support.worldwildlife.org/site/SPageServer?pagename=main_monthly&s_src=AWE2209OQ18299A01179RX&s_subsrc=topnav'
+    }, 
+]; 
+
+module.exports = organizationData; 
