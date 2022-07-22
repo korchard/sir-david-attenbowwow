507661c1a2880d9e825bc2144bc634e0586351b6
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-07-22 15:10:45 -0500

    updated reactDom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // REACT
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 
 // REDUX
 import { createStore, applyMiddleware } from 'redux';
