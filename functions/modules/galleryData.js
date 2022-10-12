48e4b7efb11a238fce458807c7b06dc1e82fd5b4
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-10-12 08:13:57 -0500

    updated gallery

--- functions/modules/galleryData.js
+++ functions/modules/galleryData.js
@@ -1,15 +1,34 @@
 const galleryData = [
     {   
         id: 1,
+        title: 'Birthday Trip to the North Shore', 
+        description: `This year I turned 2 and my paw-rents took me up the North Shore
+        of Lake Superior to see the fall colors and hike. The lake was beautiful, and 
+        also very chilly. There is something magical about this large lake that I look 
+        forward to continuing to develop a relationship with it.`, 
+        image: '../images/daveyBirthday.jpg'
+    }, 
+    {   
+        id: 2,
+        title: 'First Boundary Waters Trip', 
+        description: `I took my first trip to the Boundary Waters this year. I thrived! 
+        Since then, anytime my paw-rents use the word "BWCA", I jump and talk and ask them 
+        to take me back there. It was such a wild place and I hope to continue to enjoy 
+        it every year from here out. We saw beavers, birds, turtles, and many other creatures. 
+        The red squirrels and I were not friends.`, 
+        image: '../images/daveyBWCA.jpg'
+    }, 
+    {   
+        id: 3,
         title: 'Davey on the Water', 
         description: `I love spending time on the boat. We have a cabin on 
         Middle Eau Claire Lake in Northern Wisconson. This lake has been cared 
         for diligently by the DNR. They have eradicated invasive species and taken 
-        measures to ensure the ecosystem of the lake remains healthy. `, 
+        measures to ensure the ecosystem of the lake remains healthy.`, 
         image: '../images/daveyOnBoat.jpg'
     }, 
     {
-        id: 2,
+        id: 4,
         title: `Hiking in Sedona`, 
         description: `We hiked a few different places while visiting Sedona. My 
         parents call me a Billy Goat, as I like to scurry up the rocks without 
@@ -17,14 +36,14 @@ const galleryData = [
         image: '../images/daveySedona.jpg'
     }, 
     {
-        id: 3,
+        id: 5,
         title: 'The Volcano', 
         description: `We visited some amazing volcanic ice caves in New Mexico. 
         I got tired so my mom carried me, I am pretty happy about this.`, 
         image: '../images/daveyVolcano.jpg'
     }, 
     {
-        id: 4,
+        id: 6,
         title: 'Hiking in Wisconsin', 
         description: `My grand-pawrents took me hiking when they were watching 
         me for a few days. We visited the Trego Nature Trail along the Namekagon 
@@ -32,7 +51,7 @@ const galleryData = [
         image: '../images/daveyHiking.jpeg'
     }, 
     {
-        id: 5,
+        id: 7,
         title: `Winter Walk in the North Woods`, 
         description: `Winter is my favorite season. I love romping around in the 
         snow and face diving into snow piles. My paw-rents like to get outside 
