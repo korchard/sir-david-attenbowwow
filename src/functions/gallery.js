1d54f6b8a01f2b3c72100b5ee53afcfdde960d50
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-10-11 22:29:11 -0500

    removed functions from inside src

--- src/functions/gallery.js
+++
@@ -1,8 +0,0 @@
-const galleryData = require('./modules/galleryData');
-
-exports.handler = async function (event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({galleryData}),
-    };
-  };
--- src/functions/organizations.js
+++
@@ -1,8 +0,0 @@
-const organizationData = require('./modules/organizationData');
-
-exports.handler = async function (event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({organizationData}),
-    };
-  };
