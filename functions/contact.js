cdca107802d38c9652eb536bcefc63eb99c06714
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-10-12 13:47:54 -0500

    updated url

--- functions/contact.js
+++ functions/contact.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // NODEMAILER && POST ROUTE to receive an email 
-router.post('/api/contact', (req, res) => {
+router.post('/contact', (req, res) => {
     console.log('email', req.body);
     const data = req.body;
 
