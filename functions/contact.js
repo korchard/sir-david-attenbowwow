6860bd1b55d3d658f5696901bbb5079131ba382d
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-11-12 01:19:25 -0600

    added express

--- functions/contact.js
+++ functions/contact.js
@@ -10,48 +10,49 @@ const nodemailer = require('nodemailer');
 
 // NODEMAILER && POST ROUTE to receive an email 
 exports.handler =  async function(event, context, callback) {
-// router.post('/', (req, res) => {
-    console.log('email', req.body);
-    const data = req.body;
+    router.post('/', (req, res) => {
+        console.log('email', req.body);
+        const data = req.body;
 
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          type: "OAuth2",
-          user: process.env.EMAIL,
-          pass: process.env.PASSWORD,
-          clientId: process.env.OAUTH_CLIENTID,
-          clientSecret: process.env.OAUTH_CLIENT_SECRET,
-          refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-        },
-       });
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+            type: "OAuth2",
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+            clientId: process.env.OAUTH_CLIENTID,
+            clientSecret: process.env.OAUTH_CLIENT_SECRET,
+            refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+            },
+        });
 
-    transporter.verify((err, success) => {
-        err
-          ? console.log(err)
-          : console.log(`=== Server is ready to take messages: ${success} ===`);
-       });
-  
-    const mailOptions = {
-        from: `${data.email}`,
-        to: process.env.EMAIL,
-        subject: `${data.subject}`,
-        html: `<p>${data.message}</p>
-                <p>Thank you, <br/>
-                ${data.name}</p>
-                <p>${data.email}</p>`
-    };
+        transporter.verify((err, success) => {
+            err
+            ? console.log(err)
+            : console.log(`=== Server is ready to take messages: ${success} ===`);
+        });
+    
+        const mailOptions = {
+            from: `${data.email}`,
+            to: process.env.EMAIL,
+            subject: `${data.subject}`,
+            html: `<p>${data.message}</p>
+                    <p>Thank you, <br/>
+                    ${data.name}</p>
+                    <p>${data.email}</p>`
+        };
 
-    transporter.sendMail(mailOptions,
-        (error, response) => {
-            if (error) {
-                console.log(`Error - ${err}`);
-                res.send(error, data)
-            } else {
-                console.log(`Success!`);
-                res.send(response, data)
-            }
-            transporter.close();
+        transporter.sendMail(mailOptions,
+            (error, response) => {
+                if (error) {
+                    console.log(`Error - ${err}`);
+                    res.send(error, data)
+                } else {
+                    console.log(`Success!`);
+                    res.send(response, data)
+                }
+                transporter.close();
+        });
     });
 };
 
