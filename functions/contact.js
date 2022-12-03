81705cf88cb937995b496f77e81ef4e49c1e0d98
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-12-02 21:52:51 -0600

    back to async function

--- functions/contact.js
+++ functions/contact.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // NODEMAILER && POST ROUTE to receive an email 
-// export async function handler(event, context) {
+export async function handler(event, context) {
     // const app = express();
     // const bodyParser = require('body-parser');
     // const router = express.Router();
@@ -19,77 +19,77 @@ app.use(bodyParser.urlencoded({ extended: true }));
     // app.use(bodyParser.json());
     // app.use(bodyParser.urlencoded({ extended: true }));
 
-router.post('/', (req, res) => {
-    console.log('email', event.body);
-    const data = event.body;
+    router.post('/', (req, res) => {
+        console.log('email', event.body);
+        const data = event.body;
 
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
-        clientId: process.env.OAUTH_CLIENTID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-        },
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+            type: "OAuth2",
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+            clientId: process.env.OAUTH_CLIENTID,
+            clientSecret: process.env.OAUTH_CLIENT_SECRET,
+            refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+            },
+        });
+
+        // transporter.verify((err, success) => {
+        //     err
+        //     ? console.log(err)
+        //     : console.log(`=== Server is ready to take messages: ${success} ===`);
+        // });
+
+        const mailOptions = {
+            from: `${data.email}`,
+            to: process.env.EMAIL,
+            subject: `${data.subject}`,
+            html: `<p>${data.message}</p>
+                    <p>Thank you, <br/>
+                    ${data.name}</p>
+                    <p>${data.email}</p>`
+        };
+
+        // transporter.sendMail(mailOptions,
+        //     (error, response) => {
+        //         if (error) {
+        //             console.log(`Error - ${err}`);
+        //             res.send(error)
+        //         } else {
+        //             console.log(`Success!`);
+        //             res.send(response)
+        //         }
+        //         transporter.close();
+        // });
+        let result = await transporter.sendMail(mailOptions)
+        .then( response => {
+
+            console.log(`Success - ${response}`);
+            
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ "Success": response }, null),
+            };    
+            
+        })
+        .catch ( error => {
+            console.log(`Error - ${error}`);
+            
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ "Error": error }, null),
+            };    
+            
+        })
+        res.send(result)
     });
-
-    // transporter.verify((err, success) => {
-    //     err
-    //     ? console.log(err)
-    //     : console.log(`=== Server is ready to take messages: ${success} ===`);
-    // });
-
-    const mailOptions = {
-        from: `${data.email}`,
-        to: process.env.EMAIL,
-        subject: `${data.subject}`,
-        html: `<p>${data.message}</p>
-                <p>Thank you, <br/>
-                ${data.name}</p>
-                <p>${data.email}</p>`
-    };
-
-    // transporter.sendMail(mailOptions,
-    //     (error, response) => {
-    //         if (error) {
-    //             console.log(`Error - ${err}`);
-    //             res.send(error)
-    //         } else {
-    //             console.log(`Success!`);
-    //             res.send(response)
-    //         }
-    //         transporter.close();
-    // });
-    let result = transporter.sendMail(mailOptions)
-    .then( response => {
-
-        console.log(`Success - ${response}`);
-        
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ "Success": response }, null),
-        };    
-        
-    })
-    .catch ( error => {
-        console.log(`Error - ${error}`);
-        
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ "Error": error }, null),
-        };    
-        
-    })
-    res.send(result)
-});
 
 // app.use('/api/contact', router)
 // return serverless(app)(event, context).then(result => {
 //     return result
 // })
-// }
+}
 
 app.use('/api/contact', router);
 
