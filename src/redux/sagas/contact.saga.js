03265264df7edf7eaac737dd38023bf2fa2c8e0c
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2022-12-02 22:17:18 -0600

    req changed

--- src/redux/sagas/contact.saga.js
+++ src/redux/sagas/contact.saga.js
@@ -4,9 +4,8 @@ import { takeEvery } from 'redux-saga/effects';
 // POST ROUTE - to send an email via nodemailer
 function* sendMessage (action) {
     try {
-      yield axios.post('/api/contact', action.payload);
-      yield put(success(result.json()));
-      console.log(payload)
+      const results =yield axios.post('/api/contact', action.payload);
+      console.log(results)
     } catch (error) {
       console.log('Bad news bears...error in contact.saga POST', error);
     }
