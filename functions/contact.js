412a2d743100a28b6848ff508ba982cbea3e9592
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2023-05-01 21:59:45 -0500

    added logging to contact function

--- functions/contact.js
+++ functions/contact.js
@@ -40,6 +40,7 @@ exports.handler = async function(event, context) {
             },
         };
     } catch (error) {
+        console.log(error);
         return {
             statusCode: 500,
             body: JSON.stringify({
