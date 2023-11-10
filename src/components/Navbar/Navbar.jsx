2f769b754868fb757177b14a19fe8f0a60371785
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2023-11-09 22:04:15 -0600

    added linting formatting and precommit hook

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,26 +5,25 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-
-  return (
-    <div className='navLinks'>
-        <div>
-            <Link className='navLink' to='/about'>
-                <span className='iconBtn'>About</span>
-            </Link>
-        </div>
-        <div>
-            <Link className='navLink' to='/gallery'>
-                <span className='iconBtn'>Gallery</span>
-            </Link>
-        </div>
-        <div>
-            <Link className='navLink' to='/donate'>
-                <span className='iconBtn'>Donate</span>
-            </Link>
-        </div>
-    </div>
-  );
+	return (
+		<div className="navLinks">
+			<div>
+				<Link className="navLink" to="/about">
+					<span className="iconBtn">About</span>
+				</Link>
+			</div>
+			<div>
+				<Link className="navLink" to="/gallery">
+					<span className="iconBtn">Gallery</span>
+				</Link>
+			</div>
+			<div>
+				<Link className="navLink" to="/donate">
+					<span className="iconBtn">Donate</span>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
