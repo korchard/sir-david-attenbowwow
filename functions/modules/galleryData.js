874abb643f39e728e8c513b433f6a4c88882eb47
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2023-11-20 19:00:05 -0600

    updated pics

--- functions/modules/galleryData.js
+++ functions/modules/galleryData.js
@@ -1,15 +1,33 @@
 const galleryData = [
 	{
-		id: 1,
+		id: 10,
+		title: `Camping in Northern Minnesota`,
+		description: `We went camping this summer on the St. Croix river. We went paddle 
+		boarding, had a fire going, and made delicious food. My favorite thing to do is spend 
+		days and days living outside with my favorite people. I sure get tuckered out though.`,
+		image: '../images/daveyCamping.jpeg',
+	},
+	{
+		id: 9,
+		title: `Saving a Turtle`,
+		description: `I found a baby turtle on the road when I was walking at the 
+		cabin. He needed to be guided back towards the river to safety. I do my best to 
+		be considerate, and remember that all my favorite places to be are also home 
+		to a variety of creatures. We all live in the same ecosystem, so it's important 
+		that we take care of each other.`,
+		image: '../images/daveyTurtle.jpeg',
+	},
+	{
+		id: 8,
 		title: 'Birthday Trip to the North Shore',
 		description: `This year I turned 2 and my paw-rents took me up the North Shore
         of Lake Superior to see the fall colors and hike. The lake was beautiful, and 
-        also very chilly. There is something magical about this large lake that I look 
+        also very chilly. There is something magical about this large lake, and I look 
         forward to continuing to develop a relationship with it.`,
 		image: '../images/daveyBirthday.jpg',
 	},
 	{
-		id: 2,
+		id: 7,
 		title: 'First Boundary Waters Trip',
 		description: `I took my first trip to the Boundary Waters this year. I thrived! 
         Since then, anytime my paw-rents use the word "BWCA", I jump and talk and ask them 
@@ -19,7 +37,7 @@ const galleryData = [
 		image: '../images/daveyBWCA.jpg',
 	},
 	{
-		id: 3,
+		id: 6,
 		title: 'Davey on the Water',
 		description: `I love spending time on the boat. We have a cabin on 
         Middle Eau Claire Lake in Northern Wisconson. This lake has been cared 
@@ -29,36 +47,49 @@ const galleryData = [
 		image: '../images/daveyOnBoat.jpg',
 	},
 	{
-		id: 4,
+		id: 5,
 		title: `Hiking in Sedona`,
 		description: `We hiked a few different places while visiting Sedona. My 
         paw-rents call me a Billy Goat, as I like to scurry up the rocks without 
         any hesitation. This picture was taken hiking Cathedral Rock, which was a bit 
-        steep and I needed to ride in the backpack.`,
+        steep and I needed to ride in the backpack. Cathedral Rock is said to be a 
+		vortex, I sure felt it.`,
 		image: '../images/daveySedona.jpg',
 	},
 	{
-		id: 5,
+		id: 4,
 		title: 'The Volcano',
-		description: `We visited some amazing volcanic ice caves in New Mexico. 
-        I got tired so my mom carried me, I am pretty happy about this.`,
+		description: `We visited some amazing volcanic ice caves in New Mexico. It was 
+		erie to see green ice formed in volcanic caves, but it felt great to step into 
+		an ice box after walking for a while. I got tired so my mom carried me, I am 
+		pretty happy about this.`,
 		image: '../images/daveyVolcano.jpg',
 	},
 	{
-		id: 6,
+		id: 3,
+		title: `Great Sand Dunes National Park`,
+		description: `I am on top of the world in this picture. Also on top of a big sand 
+		mound. We camped under the stars after trekking up a long ways. My short little legs 
+		brought me to the top. This place is beautiful. The stars were a plenty and the sunrise 
+		and sunset were truly magical.`,
+		image: '../images/daveySandDunes.jpeg',
+	},
+	{
+		id: 2,
 		title: 'Hiking in Wisconsin',
 		description: `My grand-paw-rents took me hiking when they were watching 
         me for a few days. We visited the Trego Nature Trail along the Namekagon 
-        River. I love my grand-paw-rents! They spoil me.`,
+        River. There is much protected forest in this area near the cabin. I love being 
+		able to frollick around the landscape and I love my grand-paw-rents! They spoil me.`,
 		image: '../images/daveyHiking.jpeg',
 	},
 	{
-		id: 7,
+		id: 1,
 		title: `Winter Walk in the North Woods`,
 		description: `Winter is my favorite season. I love romping around in the 
         snow and face diving into snow piles. My paw-rents like to get outside 
-        often during the winter months. Here we are walking through the North 
-        woods in Minnesota.`,
+        often during the winter months, it helps all of us to be able to be outside all 
+		year round. Here we are walking through the North woods in Minnesota.`,
 		image: '../images/daveyAtCabin.jpg',
 	},
 ];
